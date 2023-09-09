1895edbdc87dca96bbe9577cc6a2dce92873a2e6
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-09-09 14:12:42 +0500

    Hero Section Created"

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,15 +6,19 @@ export default function Home() {
     <>
       <div className='hero-section'>
         <div className='overlay'>
-          <div>
-            <h1>Enjoy Our Delicious Meal</h1>
+          <div className='hero-txt'>
+            <h1>Enjoy Our <br/> Delicious Meal</h1>
             <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
             <button className='review-btn'>Review Us</button>
           </div>
-          <div>
-            <img className='hero-img' src={heroImg}/>
+          <div className='hero-img'>
+            <img src={heroImg}/>
           </div>
         </div>
+      </div>
+
+      <div className='home-cards'>
+
       </div>
     </>
   )
