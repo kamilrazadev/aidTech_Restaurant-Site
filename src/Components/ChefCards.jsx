1b9c346d28a5449875bae5140b16143ba690d766
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-09-10 23:05:36 +0500

    Menu Completed and Chefs Section Added

---
+++ src/Components/ChefCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ChefCards(props) {
+  return (
+    <div className='chef-cards'>
+          <img src={props.image} className='chef-img' />
+          <h3 className='chef-desgn'>{props.desgn}</h3>
+          <p className='chef-name'>{props.name}</p>
+          <div className='social-acc'>
+            <div>I</div>
+            <div>F</div>
+            <div>T</div>
+          </div>
+          <div className='over'></div>
+    </div>
+  )
+}
--- src/Pages/Chefs.jsx
+++ src/Pages/Chefs.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import ChefCards from '../Components/ChefCards'
 
 export default function Chefs() {
+
+  const chefsData = [
+    {
+      image: '../../public/images/team-1.jpg',
+      name: "John Doe",
+      desgn: "Head Chef"
+    },
+    {
+      image: '../../public/images/team-2.jpg',
+      name: "Trevor",
+      desgn: "Jr Chef"
+    },
+    {
+      image: '../../public/images/team-3.jpg',
+      name: "Michel Decenta",
+      desgn: "Desert Expert"
+    },
+    {
+      image: '../../public/images/team-4.jpg',
+      name: "Franklin",
+      desgn: "Chinese Expert"
+    }
+  ]
+
   return (
     <div className='chefs-section'>
       <div className='section-heading' data-aos='fade-up' >
@@ -9,6 +34,13 @@ export default function Chefs() {
         <div></div>
       </div>
       <h3 className='main-heading' data-aos='fade-up'>Our Master Chefs</h3>
+      <div className='chefs-card-contanier'>
+        {
+          chefsData.map( (val, key) => (
+            <ChefCards key={key} image={val.image} desgn={val.desgn} name={val.name} />
+          ))
+        } 
+      </div>
     </div>
   )
 }
