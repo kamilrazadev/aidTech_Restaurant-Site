d5f700637c39f1e3262bcb13fbc31ecff5d3ce68
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-09-12 12:10:23 +0500

    Nav Responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import './Component.css'
 
 export default function NavBar() {
+
+  const handleNavShow = () => {
+    
+  }
+
   return (
     <nav>
         <h3 className='brand-name'>FOODnFOODS</h3>
@@ -14,6 +19,25 @@ export default function NavBar() {
             {/* <Link className='nav-link' to='/contact-us'>Contact Us</Link> */}
             {/* <Link className='nav-link' to='/review-us'>Review Us</Link> */}
         </div>
+
+        <div className='nav-toogle'>
+          <input type="checkbox" id="checkbox" onClick={handleNavShow} />
+          <label htmlFor="checkbox" className="toggle">
+            <div className="bars" id="bar1" />
+            <div className="bars" id="bar2" />
+            <div className="bars" id="bar3" />
+            <div className='nav-links-responsive'>
+          <Link className='nav-link' to='/'>Home</Link>
+          <Link className='nav-link' to='/about'>About</Link>
+          <Link className='nav-link' to='/menu'>Menu</Link>
+          <Link className='nav-link' to='/team'>Our Team</Link>
+        </div>
+          </label>  
+          
+        </div>
+        
+        
+
     </nav>
   )
 }
