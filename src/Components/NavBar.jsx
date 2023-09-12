ecbd5228f37af9d8e0ed139636467538c740dcef
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-09-12 12:43:25 +0500

    Nav Responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Component.css'
 
 export default function NavBar() {
 
-  const handleNavShow = () => {
-    
+  const [checkboxState, setCheckboxState] = useState(false)
+
+  const handleCheckboxState = () => {
+
+    if(checkboxState == false){
+      setCheckboxState(true)
+    } else {
+      setCheckboxState(false);
+    }
+
   }
 
   return (
@@ -21,22 +29,26 @@ export default function NavBar() {
         </div>
 
         <div className='nav-toogle'>
-          <input type="checkbox" id="checkbox" onClick={handleNavShow} />
+
+          <input type="checkbox" id="checkbox" checked={checkboxState} onChange={handleCheckboxState} />
+          
           <label htmlFor="checkbox" className="toggle">
             <div className="bars" id="bar1" />
             <div className="bars" id="bar2" />
             <div className="bars" id="bar3" />
-            <div className='nav-links-responsive'>
-          <Link className='nav-link' to='/'>Home</Link>
-          <Link className='nav-link' to='/about'>About</Link>
-          <Link className='nav-link' to='/menu'>Menu</Link>
-          <Link className='nav-link' to='/team'>Our Team</Link>
-        </div>
-          </label>  
+          </label> 
           
+          <div className='nav-links-responsive' onClick={handleCheckboxState} >
+            <Link className='nav-link' to='/'>Home</Link>
+            <Link className='nav-link' to='/about'>About</Link>
+            <Link className='nav-link' to='/menu'>Menu</Link>
+            <Link className='nav-link' to='/team'>Our Team</Link>
+            {/* <Link className='nav-link' to='/contact-us'>Contact Us</Link> */}
+            {/* <Link className='nav-link' to='/review-us'>Review Us</Link> */}
+          </div> 
+        
+
         </div>
-        
-        
 
     </nav>
   )
